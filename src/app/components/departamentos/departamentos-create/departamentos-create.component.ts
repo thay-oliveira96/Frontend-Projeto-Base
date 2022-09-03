95f2ccd090b4e61d24765d1b8c51bc20740b47e4
Thaynan Oliveira <thaynan-rodrigues@outlook.com>
2022-09-03 20:25:10 -0300

    CRUD completo Departamentos

--- src/app/components/departamentos/departamentos-create/departamentos-create.component.ts
+++ src/app/components/departamentos/departamentos-create/departamentos-create.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Departamentos } from 'src/app/models/departamentos';
+import { DepartamentoService } from 'src/app/services/departamentos.service';
 
 @Component({
   selector: 'app-departamentos-create',
@@ -7,9 +12,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DepartamentosCreateComponent implements OnInit {
 
-  constructor() { }
+  departamentos: Departamentos = {
+    id:         '',
+    nome:       ''
+  }
+
+  nome: FormControl = new FormControl(null, Validators.minLength(3));
+
+  constructor(
+    private service: DepartamentoService,
+    private toast: ToastrService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  
+  create(): void {
+    this.service.create(this.departamentos).subscribe(() => {
+      this.toast.success('Departamento cadastrado com sucesso', 'cadastro');
+      this.router.navigate(['departamentos'])
+    }, ex => {
+      console.log(ex);
+      if(ex.error.errors) {
+        ex.error.errors.forEach(element => {
+          this.toast.error(element.message);
+        });
+      } else {
+        this.toast.error(ex.error.message);
+      }
+    })
+  }
+
+  validaCampos(): boolean {
+    return this.nome.valid
+  }
+  
 }
+
--- src/app/components/departamentos/departamentos-delete/departamentos-delete.component.ts
+++ src/app/components/departamentos/departamentos-delete/departamentos-delete.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Departamentos } from 'src/app/models/departamentos';
+import { DepartamentoService } from 'src/app/services/departamentos.service';
 
 @Component({
   selector: 'app-departamentos-delete',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DepartamentosDeleteComponent implements OnInit {
 
-  constructor() { }
+  departamentos: Departamentos = {
+    id:         '',
+    nome:       ''
+  }
+
+  constructor(
+    private service: DepartamentoService,
+    private toast:    ToastrService,
+    private router:          Router,
+    private route:   ActivatedRoute,
+    ) { }
 
   ngOnInit(): void {
+    this.departamentos.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
+   }
+
+  findById(): void {
+    this.service.findById(this.departamentos.id).subscribe(resposta => {
+      this.departamentos = resposta;
+    })
+  }
+
+  delete(): void {
+    this.service.delete(this.departamentos.id).subscribe(() => {
+      this.toast.success('Empresa Deletada com sucesso', 'Delete');
+      this.router.navigate(['departamentos'])
+    }, ex => {
+      if(ex.error.errors) {
+        ex.error.errors.forEach(element => {
+          this.toast.error(element.message);
+        });
+      } else {
+        this.toast.error(ex.error.message);
+      }
+    })
   }
 
 }
