c7ec9aa2dbb3375889247ec0448d234ab9da1865
Thaynan Oliveira <thaynan-rodrigues@outlook.com>
2022-09-12 16:57:08 -0300

    Implementação da tela Peças Update

--- src/app/components/pecas/pecas-update/pecas-update.component.ts
+++ src/app/components/pecas/pecas-update/pecas-update.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Pecas } from 'src/app/models/pecas';
+import { PecaService } from 'src/app/services/pecas.service';
 
 @Component({
   selector: 'app-pecas-update',
@@ -7,9 +12,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PecasUpdateComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  pecas: Pecas = {
+    id:         '',
+    nome:       ''
   }
 
+  nome: FormControl =  new FormControl(null, Validators.minLength(3));
+  
+  constructor(
+    private service: PecaService,
+    private toast:    ToastrService,
+    private router:          Router,
+    private route:   ActivatedRoute,
+    ) { }
+
+  ngOnInit(): void {
+    this.pecas.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
+   }
+
+  findById(): void {
+    this.service.findById(this.pecas.id).subscribe(resposta => {
+      this.pecas = resposta;
+    })
+  }
+
+  update(): void {
+    this.service.update(this.pecas).subscribe(() => {
+      this.toast.success('Peça atualizada com sucesso', 'Update');
+      this.router.navigate(['pecas'])
+    }, ex => {
+      if(ex.error.errors) {
+        ex.error.errors.forEach(element => {
+          this.toast.error(element.message);
+        });
+      } else {
+        this.toast.error(ex.error.message);
+      }
+    })
+  }
+  
+  validaCampos(): boolean {
+    return this.nome.valid
+  }
 }
