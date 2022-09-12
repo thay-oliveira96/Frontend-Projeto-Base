65c4ecd20db94baa758939d1f2e12b4242951d93
Thaynan Oliveira <thaynan-rodrigues@outlook.com>
2022-09-12 17:23:54 -0300

    Implementação da Tela Peças Delete

--- src/app/components/pecas/pecas-delete/pecas-delete.component.ts
+++ src/app/components/pecas/pecas-delete/pecas-delete.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Pecas } from 'src/app/models/pecas';
+import { PecaService } from 'src/app/services/pecas.service';
 
 @Component({
   selector: 'app-pecas-delete',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PecasDeleteComponent implements OnInit {
 
-  constructor() { }
+  pecas: Pecas = {
+    id:         '',
+    nome:       ''
+  }
+
+  constructor(
+    private service: PecaService,
+    private toast:    ToastrService,
+    private router:          Router,
+    private route:   ActivatedRoute,
+    ) { }
 
   ngOnInit(): void {
+    this.pecas.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
+   }
+
+  findById(): void {
+    this.service.findById(this.pecas.id).subscribe(resposta => {
+      this.pecas = resposta;
+    })
+  }
+
+  delete(): void {
+    this.service.delete(this.pecas.id).subscribe(() => {
+      this.toast.success('Peça Deletada com sucesso', 'Delete');
+      this.router.navigate(['pecas'])
+    }, ex => {
+      if(ex.error.errors) {
+        ex.error.errors.forEach(element => {
+          this.toast.error(element.message);
+        });
+      } else {
+        this.toast.error(ex.error.message);
+      }
+    })
   }
 
 }
