e726c27cf1955ca371c6a904c2ff7d0015690b96
Thaynan Oliveira <thaynan-rodrigues@outlook.com>
2022-09-12 16:25:42 -0300

    Implementação da tela Peças Create

--- src/app/components/pecas/pecas-create/pecas-create.component.ts
+++ src/app/components/pecas/pecas-create/pecas-create.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Pecas } from 'src/app/models/pecas';
+import { PecaService } from 'src/app/services/pecas.service';
 
 @Component({
   selector: 'app-pecas-create',
@@ -7,9 +12,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PecasCreateComponent implements OnInit {
 
-  constructor() { }
+  pecas: Pecas = {
+    id:         '',
+    nome:       ''
+  }
+
+  nome: FormControl = new FormControl(null, Validators.minLength(3));
+
+  constructor(
+    private service: PecaService,
+    private toast: ToastrService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  
+  create(): void {
+    this.service.create(this.pecas).subscribe(() => {
+      this.toast.success('Peça cadastrada com sucesso', 'cadastro');
+      this.router.navigate(['pecas'])
+    }, ex => {
+      console.log(ex);
+      if(ex.error.errors) {
+        ex.error.errors.forEach(element => {
+          this.toast.error(element.message);
+        });
+      } else {
+        this.toast.error(ex.error.message);
+      }
+    })
+  }
+
+  validaCampos(): boolean {
+    return this.nome.valid
+  }
+  
 }
+
